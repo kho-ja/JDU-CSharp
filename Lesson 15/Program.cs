1602da61e480a855d54ac255d48d0b2498ad7e1f
Abdulazizxoʻja Azimxoʻjayev <111680662+kho-ja@users.noreply.github.com>
2026-02-02 14:47:54 +0500

    Refactor database migration logic in Program.cs to handle exceptions and ensure database creation

--- Lesson 15/Program.cs
+++ Lesson 15/Program.cs
@@ -34,7 +34,21 @@ app.MapControllerRoute(
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    db.Database.Migrate();
+    try
+    {
+        if (db.Database.GetPendingMigrations().Any())
+        {
+            db.Database.Migrate();
+        }
+        else
+        {
+            db.Database.EnsureCreated();
+        }
+    }
+    catch (InvalidOperationException)
+    {
+        db.Database.EnsureCreated();
+    }
 }
 
 
